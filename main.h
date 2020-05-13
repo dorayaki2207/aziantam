093fb50d5511af56781dc53c37b96c24d30d5a29
山本美由紀 <1916045@s.asojuku.ac.jp>
2020-05-13 12:43:31 +0900

    1年のときに書いたプログラムをコピペした部分がありますいらない場合は変更または消してください

--- main.h
+++ main.h
@@ -20,8 +20,53 @@ enum SCENE				//ｼｰﾝ管理用
 	SCENE_GAMEOVER,		//ｹﾞｰﾑｵｰﾊﾞｰｼｰﾝ
 	SCENE_MAX
 };
+//向き管理用
+enum DIR
+{
+	DIR_UP,			//上
+	DIR_RIGHT,		//右
+	DIR_DOWN,		//下
+	DIR_LEFT,		//左
+	DIR_MAX
+};
+//ｷｬﾗｸﾀの種類管理用
+//enum CHARACTER_TYPE
+//{
+//	CHARACTER_PLAYER,
+//	CHARACTER_ENEMY,
+//	CHARACTER_SHOT,
+//	CHARACTER_MAX,
+//};
 
 //---構造体
+//----------------
+//得点管理用
+struct FILE_DATA
+{
+	int Score;				//現在得点
+	int hiScore;			//最高得点
+};
+//ポジション
+struct XY
+{
+	int x;
+	int y;
+};
+//ｷｬﾗｸﾀ
+struct CHARACTER
+{
+	int CharType;	//ｷｬﾗｸﾀの種類
+	DIR MoveDir;	//ｷｬﾗｸﾀの向き
+	XY Pos;			//ｷｬﾗｸﾀの地図上の座標
+	XY Size;		//ｷｬﾗｸﾀ画像のｻｲｽﾞ
+	XY OffsetSize;	//ｷｬﾗｸﾀのｵﾌｾｯﾄ
+	int MoveSpeed;	//ｷｬﾗｸﾀの移動ｽﾋﾟｰﾄﾞ
+	int LifeMax;	//ｷｬﾗｸﾀの体力最大値
+	int Life;		//ｷｬﾗｸﾀの体力
+	int AnimCnt;	//ｷｬﾗｸﾀのｱﾆﾒｰｼｮﾝ
+
+	int point;		//ｷｬﾗｸﾀの得点
+};
 
 //---プロトタイプ宣言
 bool SystemInit(void);
