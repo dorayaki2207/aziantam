51d78923a565cecbde41133a9137c5be67fd0dad
山本美由紀 <1916045@s.asojuku.ac.jp>
2020-05-15 11:54:37 +0900

    アジアンタムプロジェクト

---
+++ aziantamProject/keycheck.cpp
@@ -0,0 +1,70 @@
+// keyheck.cpp
+// 2019.09.20
+// 山本美由紀
+
+#include "DxLib.h"
+#include "keycheck.h"
+
+//変数
+//ｰｰｰｰｰｰｰｰｰ
+//入力情報関連
+bool KeyOld[KEY_ID_MAX];			//1フレーム前の入力情報
+bool KeyNew[KEY_ID_MAX];			//現在の入力情報
+bool KeyDownTrigger[KEY_ID_MAX];	//ダウン時のトリガー
+bool KeyUpTrigger[KEY_ID_MAX];		//アップ時のトリガー
+
+
+//入力情報の初期化
+void KeyInit(void)
+{
+	for (int k = 0; k < KEY_ID_MAX; k++)
+	{
+		KeyOld[k] = false;
+		KeyNew[k] = false;
+		KeyDownTrigger[k] = false;
+		KeyUpTrigger[k] = false;
+	}
+}
+//入力情報の確認
+void KeyCheck(void)
+{
+	//int mouseButton;					//マウスのボタン状態
+
+	//Oldの準備
+	for (int KeyID = 0; KeyID < KEY_ID_MAX; KeyID++)
+	{
+		KeyOld[KeyID] = KeyNew[KeyID];
+	}
+
+	//Mouseの情報を取得する
+	//GetMousePoint(&mousePos.x, &mousePos.y);
+	//mouseButton = GetMouseInput();
+
+	//現在の状態を取得する
+	KeyNew[KEY_ID_SPACE] = CheckHitKey(KEY_INPUT_SPACE);
+	KeyNew[KEY_ID_PAUSE] = CheckHitKey(KEY_INPUT_P);
+	KeyNew[KEY_ID_SHOT] = CheckHitKey(KEY_INPUT_LCONTROL);
+	KeyNew[KEY_ID_SAVE] = CheckHitKey(KEY_INPUT_S);
+	KeyNew[KEY_ID_LOAD] = CheckHitKey(KEY_INPUT_L);
+	KeyNew[KEY_ID_UP] = CheckHitKey(KEY_INPUT_UP);
+	KeyNew[KEY_ID_RIGHT] = CheckHitKey(KEY_INPUT_RIGHT);
+	KeyNew[KEY_ID_DOWN] = CheckHitKey(KEY_INPUT_DOWN);
+	KeyNew[KEY_ID_LEFT] = CheckHitKey(KEY_INPUT_LEFT);
+	for (int keyID = 0; keyID < KEY_ID_MAX; keyID++)
+	{
+		KeyDownTrigger[keyID] = false;
+		KeyUpTrigger[keyID] = false;
+		//ダウン時のトリガーの算出
+		if (!KeyOld[keyID] && KeyNew[keyID])
+		{
+			KeyDownTrigger[keyID] = true;
+		}
+		//アップ時のトリガーの算出
+		if (KeyOld[keyID] && !KeyNew[keyID])
+		{
+			KeyUpTrigger[keyID] = true;
+		}
+	}
+
+
+}
---
+++ aziantamProject/main.cpp
@@ -0,0 +1,116 @@
+#include "DxLib.h"
+#include "main.h"
+
+//•Ï”
+SCENE SceneID;
+SCENE ScenePreID;	//‰ß‹‚Ì¼°İŠi”[—p
+int SceneCounter;
+
+//¸×½‚©‚ç²İ½Àİ½‚ğ¶¬
+
+
+//WinŠÖ”
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+
+	if (!SystemInit())
+	{
+		return -1;
+	}
+	//°°°°°°°¹Ş°ÑÙ°Ìß
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		//¼°İ¶³İÄØ¾¯Ä
+		if (SceneID != ScenePreID)
+		{
+			ScenePreID = SceneID;
+			SceneCounter = 0;
+		}
+		//°°°°‰æ–Ê•`‰æˆ—
+		ClsDrawScreen();	//‰æ–ÊÁ‹
+		//¼°İ‘I‘ğ
+		switch (SceneID)
+		{
+		case SCENE_INIT:
+			InitScene();
+			break;
+		case SCENE_TITLE:
+			TitleScene();
+			break;
+		case SCENE_GAME:
+			GameScene();
+			break;
+		case SCENE_GAMEOVER:
+			GameOverScene();
+			break;
+		default:
+			return -1;
+			break;
+		}
+		SceneCounter++;
+		ScreenFlip();		//@— ‰æ–Ê‚ğ•\‰æ–Ê‚ÉuŠÔºËß°
+	}
+	//²İ½Àİ½‚Ì‰ğ•ú
+
+
+	DxLib_End();			// DX×²ÌŞ×Ø‚ÌI—¹ˆ—
+	return 0;				// ‚±‚ÌÌßÛ¸Ş×Ñ‚ÌI—¹
+
+}
+//¼½ÃÑ‚Ì‰Šú‰»
+bool SystemInit(void)
+{
+	//--------¼½ÃÑˆ—
+	SetWindowText("Œ–é`O‚Â‚Ì_•ó`");
+	SetGraphMode(SCREEN_SIZE_X, SCREEN_SIZE_Y, 16);
+	ChangeWindowMode(true);
+	if (DxLib_Init() == -1)return false;
+	SetDrawScreen(DX_SCREEN_BACK);
+	
+	//²İ½Àİ½‚Ì¶¬
+
+	//¸Ş×Ì¨¯¸“o˜^
+
+	//•Ï”‚Ì‰Šú‰»
+	SceneCounter = 0;
+	SceneID = SCENE_INIT;
+	ScenePreID = SCENE_MAX;
+
+	return true;
+}
+//‰Šú‰»¼°İ
+void InitScene(void)
+{
+	
+}
+//À²ÄÙ¼°İ
+void TitleScene(void)
+{
+	TitleDraw();
+}
+//À²ÄÙ‚Ì•`‰æ
+void TitleDraw(void)
+{
+
+}
+
+//¹Ş°Ñ¼°İ
+void GameScene(void)
+{
+	GameDraw();
+}
+//¹Ş°Ñ‚Ì•`‰æ
+void GameDraw(void)
+{
+
+}
+//¹Ş°Ñµ°ÊŞ°¼°İ
+void GameOverScene(void)
+{
+	GameOverDraw();
+}
+//¹Ş°Ñµ°ÊŞ°‚Ì•`‰æ
+void GameOverDraw(void)
+{
+
+}
