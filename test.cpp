1363d0169b9d809424b0935d4a72086fceaa917b
どら焼き <yuriyuka2207@yahoo.co.jp>
2020-05-15 10:18:33 +0900

    テキストファイルを生成、書き込み　　アイテム素材サイズ確認

--- test.cpp
+++ test.cpp
@@ -18,9 +18,15 @@ int SceneCounter;			//	gameLoop“®ìŠm”F—p
 SCENE_ID sceneID;
 SCENE_ID preSceneID;
 
+int BImage;
+int RImage;
+int PImage;
+int GImage;
 
-int testImage;
-
+int BBImage;
+int RRImage;
+int PPImage;
+int GGImage;
 //-----WinMain
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
@@ -100,8 +106,15 @@ bool SystemInit(void)
 	if (DxLib_Init() == -1) return -1;
 	SetDrawScreen(DX_SCREEN_BACK);
 
-	testImage = LoadGraph("ŒäDˆÄ/B_small_B.png");
+	BImage = LoadGraph("ŒäDˆÄ/B_small.png");
+	RImage = LoadGraph("ŒäDˆÄ/R_small.png");
+	PImage = LoadGraph("ŒäDˆÄ/P_small.png");
+	GImage = LoadGraph("ŒäDˆÄ/G_small.png");
 
+	BBImage = LoadGraph("ŒäDˆÄ/B.png");
+	RRImage = LoadGraph("ŒäDˆÄ/R.png");
+	PPImage = LoadGraph("ŒäDˆÄ/P.png");
+	GGImage = LoadGraph("ŒäDˆÄ/G.png");
 	//-----•Ï”‚Ì‰Šú‰»
 	SceneCounter = 0;
 	
@@ -142,8 +155,18 @@ void GameDraw(void)
 
 	//-----î•ñˆ—
 	DrawFormatString(0, 0, 0xFFFFFF, "GameMain : %d", SceneCounter);
+	DrawBox(0, 0, SCREEN_SIZE_X, SCREEN_SIZE_Y, 0xFFFFFF, true);
 
 	DrawBox(15, SCREEN_SIZE_Y - 220, SCREEN_SIZE_X - 15, SCREEN_SIZE_Y - 5, 0xFF22FF, false);
-	DrawGraph(50, 50, testImage, true);
+	DrawGraph(30, 50, BImage, true);
+	DrawGraph(60, 50, RImage, true);
+	DrawGraph(90, 50, PImage, true);
+	DrawGraph(120, 50, GImage, true);
+
+	DrawGraph(30, 80, BBImage, true);
+	DrawGraph(60, 80, RRImage, true);
+	DrawGraph(90, 80, PPImage, true);
+	DrawGraph(120, 80, GGImage, true);
+
 }
 
