8109b85240db6270c285ba3ac8cc837311b3c5e2
どら焼き <yuriyuka2207@yahoo.co.jp>
2020-05-19 01:23:05 +0900

    イベントリの試作

--- ISHIBASHI_test/test.cpp
+++ ISHIBASHI_test/test.cpp
@@ -7,27 +7,42 @@
 //////////	drawtext
 //////////	%s 引数を文字列のポインタとして扱い、文字列から"\0"を検出
 //////////   するか、精度で指定した文字列に達するまで文字を出力
+//////////	%c一文字ずつ
+//////////	%if キーボード入力した文字列をへんかん？　double型に変換
 
 
 #include "DxLib.h"
+#include "stdio.h"
 #include "test.h"
-
+#include "KeyCheck.h"
 
 
 //-----外部変数宣言
 int SceneCounter;			//	gameLoop動作確認用
 SCENE_ID sceneID;
 SCENE_ID preSceneID;
+//PAUSE
+int pCnt;
+bool pauseFlag;
+//御札
+int hiImage;
+int hiCnt;
+int mizuImage;
+int mizuCnt;
+int kazeImage;
+int kazeCnt;
+int kaifukuImage;
+int kaifukuCnt;
 
-struct FILE_DATA
-{
-	int data1;
-	int data2;
-};
-char fileName[] = "data.txt";
-struct FILE_DATA fileData;
 
+//会話システム
 const char *file;
+char words[200];
+
+//イベントリ
+int totalScrNew;
+int totalScr[SCR_MAX];
+
 
 //-----WinMain
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
@@ -40,6 +55,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	while (ProcessMessage() == 0 && !CheckHitKey(KEY_INPUT_ESCAPE))
 	{
 		ClsDrawScreen();
+
+		//-----ｷｰの状態取得
+		KeyCheck();
 
 
 		//-----ｼｰﾝｶｳﾝﾀ初期化
@@ -56,31 +74,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		case SCENE_ID_INIT:
 			InitScene();
 			break;
-
-			//	//ﾀｲﾄﾙｼｰﾝ
-			//case SCENE_ID_TITLE:
-			//	TitleScene();
-			//	break;
-
-			//	//ｾﾚｸﾄｼｰﾝ
-			//case SCENE_ID_SELECT:
-			//	T_selectScene();
-			//	break;
-
-			//	//ﾛｰﾄﾞｼｰﾝ
-			//case SCENE_ID_LOAD:
-			//	LoadScene();
-			//	break;
+		
+			//ﾀｲﾄﾙｼｰﾝ
+		case SCENE_ID_TITLE:
+			TitleScene();
+			break;
 
 			//ｹﾞｰﾑｼｰﾝ
 		case SCENE_ID_GAME:
 			GameScene();
 			break;
-
-			//	//ｹﾞｰﾑｵｰﾊﾞｰｼｰﾝ
-			//case SCENE_ID_GAMEOVER:
-			//	GameOverScene();
-			//	break;
 
 		default:
 			return -1;
@@ -109,12 +112,51 @@ bool SystemInit(void)
 	SetDrawScreen(DX_SCREEN_BACK);
 
 
+	//-----入力情報初期化
+	KeyInit();
+
+
+	//-----ｸﾞﾗﾌｨｯｸ登録
+	hiImage = LoadGraph("御札案/R_small.png");
+	mizuImage = LoadGraph("御札案/B_small.png");
+	kazeImage = LoadGraph("御札案/G_small.png");
+	kaifukuImage = LoadGraph("御札案/P_small.png");
+
+
 	//-----変数の初期化
 	SceneCounter = 0;
 	
-	fileData.data1 = 256;
-	fileData.data2 = 256;
+	//PAUSE関連
+	pCnt = 0;
+	pauseFlag = false;
+	FILE* fp;
+	// ﾌｧｲﾙ読み込み失敗した場合 0以外
+	if (fopen_s(&fp, "scr.dat", "w+") != NULL)
+	{
+		//ｸﾘｯｸ数の初期化
+		// 大きい値を入れて更新していく
+		for (int j = 0; j < SCR_MAX; j++)
+		{
+			// ﾀﾞﾐｰの初期化
+			totalScr[j] = 0;
 
+		}
+	}
+	// ﾌｧｲﾙ読み込み成功
+	else
+	{
+		//書き込みたいｱﾄﾞﾚｽの先頭ｱﾄﾞﾚｽ
+		fread(
+			totalScr,
+			sizeof(totalScr[0]),
+			SCR_MAX,
+			fp
+		);
+		// 失敗している場合再度閉じる必要がないからこの位置で閉じる
+		fclose(fp);
+	}
+	
+	
 	file = "dafafile.txt";
 
 	return retFlag;
@@ -124,41 +166,184 @@ bool SystemInit(void)
 void InitScene(void)
 {
 	//-----各ｵﾌﾞｼﾞｪｸﾄ処理
-	sceneID = SCENE_ID_GAME;
+	//御札枚数用
+	hiCnt = 0;
+	mizuCnt = 0;
+	kazeCnt = 0;
+	kaifukuCnt = 0;
+
+
+
+
+	for (int j = 0; j < SCR_MAX; j++)
+	{
+		if (totalScrNew > totalScr[j])
+		{
+			if (j < SCR_MAX - 1)
+			{
+				for (int f = SCR_MAX - 2; f >= j; f--)
+				{
+					totalScr[j + 1] = totalScr[j];
+				}
+			}
+			totalScr[j] = totalScrNew;
+			break;
+		}
+	}
+	FILE* fp = NULL;
+	//	ﾌｧｲﾙﾎﾟｲﾝﾀ、ﾌｧｲﾙ名、ﾌｧｲﾙｵｰﾌﾟﾝ形式
+	// ﾌｧｲﾙが開けたときの処理
+	if (fopen_s(&fp, "scr.dat", "w+") == 0)
+	{
+		// 配列の先頭を渡す、
+		fwrite(
+			totalScr,
+			sizeof(totalScr[0]),
+			SCR_MAX,
+			fp
+		);
+		fclose(fp);
+	}
+
+
+	sceneID = SCENE_ID_TITLE;
 }
 
-void GameScene(void)
+
+void TitleScene()
 {
-	
 
+	if (keyDownTrigger[KEY_ID_SPACE])
+	{
+		sceneID = SCENE_ID_GAME;
+	}
+
+	DrawFormatString(0, 50, 0xFFFFFF, "Title:%d", SceneCounter);
+}
+
+
+void GameScene()
+{
+	if (keyDownTrigger[KEY_ID_SPACE])
+	{
+		sceneID = SCENE_ID_INIT;
+	}
+
+	
+	//-----FILE操作
 	FILE *fp;
-	char words[200];
+	
 	fopen_s(&fp, file, "r");
-	fscanf(fp,  words,"%s");
-//	fgets(&file, sizeof(words), fp);
+//	fscanf_s(fp,"%s",words,256);
+//	fgets(words, 256, fp);
+//	printf("\n", words);	
+	fread(words, sizeof(words), size_t(2), fp);
+//	fprintf(fp, "HelloWorld!!!!むずかしいなぁ");
 
-	//fread(&fileData, sizeof(fileName), size_t(2), fp);
-	
-	
-//	fprintf(fp, "HelloWorld!!!!");
-	//fwritez(&fileData, sizeof(fikeName), size_t(2), fp);
+	for (int a = 0; a < words[a]; a++)
+	{
+		if (CheckHitKey(KEY_INPUT_A))
+		{
+			printf("%s\n", words);
+		}
+	}
 
 	fclose(fp);
+	
+	
+	//-----PAUSE機能
+	//ｷｰ処理
+	if (keyDownTrigger[KEY_ID_PAUSE])
+	{
+		pauseFlag = !pauseFlag;
 
+	}
+
+	if (pauseFlag)
+	{
+		SetDrawBright(128, 128, 128);
 	
 
+
+	}
+	else
+	{
+		//-----通常時動作
+		//各種機能
+		pCnt++;
+	
+		if (keyDownTrigger[KEY_ID_UP])hiCnt++;
+		if (keyDownTrigger[KEY_ID_RIGHT])mizuCnt++;
+		if (keyDownTrigger[KEY_ID_DOWN])kazeCnt++;
+		if (keyDownTrigger[KEY_ID_LEFT])kaifukuCnt++;
+
+	}
+
+
+
+
+
 	GameDraw();
+	
+
 }
 
 
-void GameDraw(void)
+void GameDraw()
 {
 
 	//-----情報処理
 	DrawFormatString(0, 0, 0xFFFFFF, "GameMain : %d", SceneCounter);
-//	DrawBox(0, 0, SCREEN_SIZE_X, SCREEN_SIZE_Y, 0xFFFFFF, true);
+//	DrawBox(0, 0, SCREEN_SIZE_X, SCREEN_SIZE_Y, 0x55FF55, true);
 
-	DrawBox(15, SCREEN_SIZE_Y - 220, SCREEN_SIZE_X - 15, SCREEN_SIZE_Y - 5, 0xFF22FF, false);
 	
+	DrawBox(15, SCREEN_SIZE_Y - 220, SCREEN_SIZE_X - 15, SCREEN_SIZE_Y - 5, 0xFF22FF, false);
+
+	//-----PAUSE関連
+	DrawFormatString(0, 12, 0xFFFFFF, "pCnt : %d", pCnt);
+
+	if (pauseFlag)
+	{
+		SetDrawBright(255, 255, 255);
+		DrawBox(100, 100, 700, 500, 0xFFFFFF, true);
+//		DrawString((SCREEN_SIZE_X - 9 * 8) / 2, (SCREEN_SIZE_Y - 16) / 2, "PAUSE", 0xFFFFFF);
+
+		//御札
+		DrawGraph(350, 250, hiImage, true);
+		DrawFormatString(380, 254, 0xFF22FF, "＠", true);
+		DrawFormatString(410, 253, 0xFF22FF, "%d", hiCnt);
+
+		DrawGraph(350, 300, mizuImage, true);
+		DrawFormatString(380, 304, 0xFF22FF, "＠", true);
+		DrawFormatString(410, 303, 0xFF22FF, "%d", mizuCnt);
+
+		DrawGraph(350, 350, kazeImage, true);
+		DrawFormatString(380, 354, 0xFF22FF, "＠", true);
+		DrawFormatString(410, 353, 0xFF22FF, "%d", kazeCnt);
+
+		DrawGraph(350, 400, kaifukuImage, true);
+		DrawFormatString(380, 404, 0xFF22FF, "＠", true);
+		DrawFormatString(410, 403, 0xFF22FF, "%d", kaifukuCnt);
+
+
+	}
+	else
+	{
+		//通常時動作
+		DrawFormatString(0, 32, 0xFFFFFF, "%s\n", words);
+
+		DrawFormatString(0, 150, 0xFF22FF, "%d", hiCnt);
+		DrawFormatString(0, 165, 0xFF22FF, "%d", mizuCnt);
+		DrawFormatString(0, 177, 0xFF22FF, "%d", kazeCnt);
+		DrawFormatString(0, 189, 0xFF22FF, "%d", kaifukuCnt);
+
+
+	}
+
+		
+
+
 }
+
+
 
