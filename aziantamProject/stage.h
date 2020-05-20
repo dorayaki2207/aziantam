e42e1d4949785c0c575bd24fbb4879b5c833cac2
HibikiAraki <milkyway.hbk@gmail.com>
2020-05-20 14:18:30 +0900

    変更

--- aziantamProject/stage.h
+++ aziantamProject/stage.h
@@ -1,45 +1,47 @@
 //月夜
 //stage.h
-//2020.05.13
+//2020.05.20
 
-//#pragma once
+//#pragma  once
 
 //定数
-#define MAP_X 25				//マップチップ画像の横サイズ
-#define MAP_Y 25				//マップチップ画像の縦サイズ
+//ﾏｯﾌﾟﾁｯﾌﾟ画像
 #define MAP_X 25
 #define MAP_Y 25
-#define MAP_CHIP_X 9
-#define MAP_CHIP_Y 9
+#define MAPI_X 25
+#define MAPI_Y 25
+#define MAPY_X 25
+#define MAPY_Y 25
+#define MAPA_X 25
+#define MAPA_Y 25
 
-//イベントID
+//ｲﾍﾞﾝﾄID
 enum STAGE_ID {
 	STAGE_ID_START,
-	
 	STAGE_ID_MAX
 };
 
-//イベント情報
+//ｲﾍﾞﾝﾄ情報
 enum EVENT_ID {
 	EVENT_ID_NON,
-	
+	EVENT_ID_HONO,
+	EVENT_ID_MIZU,
 	EVENT_ID_MAX
 };
 
 //extern宣言
-//extern int map[MAP_Y][MAP_X];
 extern XY mapPos;
-extern STAGE_ID stageID;		//ステージID
+extern STAGE_ID stageID;		//ｽﾃｰｼﾞID
 
-//プロトタイプ宣言
-void StageSystemInit(void);			//ステージ情報の初期化
-void StageGameInit(void);			//ステージ情報のゲーム初期化
-void StageGameDraw(void);			//ゲーム画面の表示処理
+//ﾌﾟﾛﾄﾀｲﾌﾟ宣言
+void StageSystemInit(void);					//ｽﾃｰｼﾞ情報の初期化
+void StageGameInit(void);					//ｽﾃｰｼﾞ情報のｹﾞｰﾑ初期化
+void StageGameDraw(void);					//ｹﾞｰﾑ画面の表示処理
 
-XY Pos2Index(XY pos);				//ピクセル座標系からマップ配列座標系に変換する。
+XY Pos2Index(XY pos);						//ﾋﾟｸｾﾙ座標系ｶﾗﾏｯﾌﾟ配列系ﾆ変換する
 
-bool IsPass(XY pos);				//指定した座標が通過可能かを返す　true:通過できる
+bool IsPass(XY pos);						//指定した座標が通過可能かを返す true:通過できる
 
-EVENT_ID GetEvent(XY pos);			//指定した座標が特別にイベントを起こすかを確認する　戻り値：イベントID
+EVENT_ID GetEvent(XY pos);					//指定した座標が特別にｲﾍﾞﾝﾄを起こすかを確認する 戻り値:ｲﾍﾞﾝﾄID
 
-void SetMapData(STAGE_ID stageID);	//ステージデータをマップ配列にコピーする
+void SetMapData(STAGE_ID stageID);			//ｽﾃｰｼﾞﾃﾞｰﾀをﾏｯﾌﾟ配列ﾆｺﾋﾟｰする
