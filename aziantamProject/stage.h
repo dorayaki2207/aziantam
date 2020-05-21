795f83518ed2378aa46fde4981ec533f9211de52
荒木響稀 <65156435+Arakihbk@users.noreply.github.com>
2020-05-21 22:26:07 +0900

    Update stage.h

--- aziantamProject/stage.h
+++ aziantamProject/stage.h
@@ -1,45 +1,56 @@
 //月夜
 //stage.h
-//2020.05.13
+//2020.05.20
 
-//#pragma once
+//#pragma  once
 
-////定数
-//#define MAP_X 25				//マップチップ画像の横サイズ
-//#define MAP_Y 25				//マップチップ画像の縦サイズ
-//#define MAP_X 25
-//#define MAP_Y 25
-//#define MAP_CHIP_X 9
-//#define MAP_CHIP_Y 9
-//
-////イベントID
-//enum STAGE_ID {
-//	STAGE_ID_START,
-//	
-//	STAGE_ID_MAX
-//};
-//
-////イベント情報
-//enum EVENT_ID {
-//	EVENT_ID_NON,
-//	
-//	EVENT_ID_MAX
-//};
-//
-////extern宣言
-////extern int map[MAP_Y][MAP_X];
-//extern XY mapPos;
-//extern STAGE_ID stageID;		//ステージID
-//
-////プロトタイプ宣言
-//void StageSystemInit(void);			//ステージ情報の初期化
-//void StageGameInit(void);			//ステージ情報のゲーム初期化
-//void StageGameDraw(void);			//ゲーム画面の表示処理
-//
-//XY Pos2Index(XY pos);				//ピクセル座標系からマップ配列座標系に変換する。
-//
-//bool IsPass(XY pos);				//指定した座標が通過可能かを返す　true:通過できる
-//
-//EVENT_ID GetEvent(XY pos);			//指定した座標が特別にイベントを起こすかを確認する　戻り値：イベントID
-//
-//void SetMapData(STAGE_ID stageID);	//ステージデータをマップ配列にコピーする
+//定数
+//ﾏｯﾌﾟﾁｯﾌﾟ画像
+#define MAP_X 25
+#define MAP_Y 25
+#define MAPI_X 25
+#define MAPI_Y 25
+#define MAPY_X 25
+#define MAPY_Y 25
+#define MAPA_X 25
+#define MAPA_Y 25
+#define MAPB_X 25
+#define MAPB_Y 25
+#define MAP_CHIP_X 9
+#define MAP_CHIP_Y 9
+#define CHIP_SIZE_X 32
+#define CHIP_SIZE_Y 32
+
+//ｲﾍﾞﾝﾄID
+enum STAGE_ID {
+	STAGE_ID_START,
+	STAGE_ID_ONI,
+	STAGE_ID_TENGU,
+	STAGE_ID_KAPPA,
+	STAGE_ID_MAX
+};
+
+//ｲﾍﾞﾝﾄ情報
+enum EVENT_ID {
+	EVENT_ID_NON,
+	EVENT_ID_HONO,
+	EVENT_ID_MIZU,
+	EVENT_ID_MAX
+};
+
+//extern宣言
+extern XY mapPos;
+extern STAGE_ID stageID;		//ｽﾃｰｼﾞID
+
+//ﾌﾟﾛﾄﾀｲﾌﾟ宣言
+void StageSystemInit(void);					//ｽﾃｰｼﾞ情報の初期化
+void StageGameInit(void);					//ｽﾃｰｼﾞ情報のｹﾞｰﾑ初期化
+void StageGameDraw(void);					//ｹﾞｰﾑ画面の表示処理
+
+XY Pos2Index(XY pos);						//ﾋﾟｸｾﾙ座標系ｶﾗﾏｯﾌﾟ配列系ﾆ変換する
+
+bool IsPass(XY pos);						//指定した座標が通過可能かを返す true:通過できる
+
+EVENT_ID GetEvent(XY pos);					//指定した座標が特別にｲﾍﾞﾝﾄを起こすかを確認する 戻り値:ｲﾍﾞﾝﾄID
+
+void SetMapData(STAGE_ID stageID);			//ｽﾃｰｼﾞﾃﾞｰﾀをﾏｯﾌﾟ配列ﾆｺﾋﾟｰする
