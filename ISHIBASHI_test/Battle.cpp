b9e45fcb610133f7526a358e66a36bf280bfe3a9
どら焼き <yuriyuka2207@yahoo.co.jp>
2020-05-24 17:27:43 +0900

    Battle.cpp入れ直し

---
+++ ISHIBASHI_test/Battle.cpp
@@ -0,0 +1,72 @@
+#include "DxLib.h"
+#include "test.h"
+#include "KeyCheck.h"
+#include "Item.h"
+#include "Battle.h"
+
+int testbkImage;
+int tBossImage[8];
+int bossCnt;
+
+void BattleSystmeInit(void)
+{
+	//-----¸Ş×Ì¨¯¸‚Ì“o˜^
+	testbkImage = LoadGraph("aitem/bg.jpg");
+	LoadDivGraph("aitem/boss.png", 8, 4, 2, 400, 500, tBossImage);
+
+	//-----‰Šú‰»
+	bossCnt = 0;
+}
+
+void BattleGameInit(void)
+{
+}
+
+
+void BattleScene(void)
+{
+
+	if (keyDownTrigger[KEY_ID_SPACE])
+	{
+		sceneID = SCENE_ID_INIT;
+	}
+	ItemControl();
+	BattleControl();
+	//•`‰æˆ—
+	BattleGameDraw();
+}
+
+
+void BattleControl(void)
+{
+
+
+
+
+
+}
+
+
+
+void BattleGameDraw(void)
+{
+	//-----•`‰æˆ—
+	//”wŒi
+	DrawGraph(0, 0, testbkImage, true);
+	//“G
+	bossCnt++;
+	DrawGraph((SCREEN_SIZE_X - 400) / 2, (BOX_Y - 500) / 2, tBossImage[(bossCnt / 16) % 8], true);
+
+	//-----î•ñˆ—
+	DrawFormatString(0, 0, 0xFFFFFF, "Battle : %d", SceneCounter);
+
+	SetDrawBright(200, 200, 200);
+	DrawBox(BOX_X, BOX_Y, BOX_END_X, BOX_END_Y, 0xFFFFFF, true);
+	SetDrawBright(255, 255, 255);
+	DrawBox(BOX_X, BOX_Y, BOX_END_X, BOX_END_Y, 0xFF22FF, false);
+
+
+	//±²ÃÑ
+	ItemB_Draw();
+
+}
---
+++ ISHIBASHI_test/Battle.h
@@ -0,0 +1,16 @@
+//#pragma once
+
+//-----’è”’è‹`
+#define BOX_X		15
+#define BOX_Y		SCREEN_SIZE_Y - 220
+#define BOX_END_X	SCREEN_SIZE_X - 15
+#define BOX_END_Y	SCREEN_SIZE_Y - 5
+
+
+//-----ŠÖ”ÌßÛÄÀ²ÌßéŒ¾
+void BattleSystmeInit(void);		//@ÎŞ½íî•ñ‚Ì‰Šú‰»	
+void BattleGameInit(void);			//						
+void BattleControl(void);			//	ÎŞ½í‚Ì‘€ì
+void BattleScene(void);				//	ÎŞ½í‚Ì¼°İ
+void BattleGameDraw(void);			//@ÎŞ½í‚Ì•`‰æˆ—
+
