e6b3b9c92544ba2cc26bff9e5b4a98a4a23d3bd2
HibikiAraki <milkyway.hbk@gmail.com>
2020-05-26 00:45:18 +0900

    追加

---
+++ aziantamProject/shot.cpp
@@ -0,0 +1,22 @@
+#include "DxLib.h"
+#include "main.h"
+#include "stage.h"
+#include "shot.h"
+
+//èâä˙âª
+void ShoSystemInit(void)
+{
+
+}
+void ShotGameInit(void)
+{
+
+}
+void ShotControl(void)
+{
+
+}
+void ShotDraw(void)
+{
+
+}
