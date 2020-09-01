c9e71c696a185f61a4107fabe2fb1d0a6a999243
Araki <moon707.piano@icloud.com>
2020-09-01 16:33:47 +0900

    a

--- YAMAMOTO_test/aziantamProject/title.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-//-----ÌßÛÄÀ²ÌßéŒ¾
-void TitleSystemInit(void);		//‰Šú‰»
-void TitleScene(void);			//À²ÄÙ‰æ–Ê
-void TitleControl(void);		//À²ÄÙ“à‘€ì
-void TitleDraw(void);			//g—p‘fŞ•`‰æˆ—
